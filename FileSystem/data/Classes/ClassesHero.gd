35a2155ff3490bb361c8b2be2ed87ab63050dc90
Wieteski <58888738+Wieteski15@users.noreply.github.com>
2026-01-20 20:27:42 +0100

    Inicjacja projektus

---
+++ FileSystem/data/Classes/ClassesHero.gd
@@ -0,0 +1,12 @@
+extends Resource
+class_name ClassesHero
+
+@export var Name: String
+#==== Statystyki ====
+@export var max_hp: int
+@export var hp: int
+@export var max_mp: int
+@export var mp: int
+@export var attack: int
+@export var defense: int
+@export var speed: int
